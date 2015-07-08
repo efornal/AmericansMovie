7027829ae2e2c9675848f25653bc2e339840434a
Ramiro Flappo <ramiro.flappo@gmail.com>
2015-07-08 11:19:32 -0300

    load some values for testing porpuses

--- testing.rb
+++ testing.rb
@@ -1,3 +1,4 @@
+require_relative "data.rb"
 #Socio.auto_migrate!
 #Pelicula.auto_migrate!
 #Prestamo.auto_migrate!
@@ -8,5 +9,4 @@ peli.director = "Maneglia, Juan Carlos & Schembori, Tana"
 peli.origen = "Paraguay, 2012"
 peli.duracion = 105
 peli.codigo = 23
-
 peli.save
